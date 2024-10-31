82151cf5b12a1742d52841b48f2b3ec4149b9292
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-10-31 13:21:41 +0600

    knowledge-cafe start with react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+
+  return (
+    <>
+
+      <div className='max-w-7xl mx-auto'>
+         <Header></Header>
+      </div>
+
+    </>
+  )
+}
+
+export default App
